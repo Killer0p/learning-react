9c413b8d2de460e091ba5fd58ed2eaa9ca1daa11
KillerOp <ribeshraut@gmail.com>
2025-06-19 21:44:17 +0545

    Making customReact and learn it works in React or not and do some code

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,13 +1,13 @@
 import Chai from './chai.jsx';
 
 function App() {
-  
+  const username = "ribesh Raut"
 
   return (
   //  <h1>Ribesh Raut</h1>
   <>
 <Chai/>
-    <h1>Ribesh Raut</h1>
+    <h1>Ribesh Raut {username} </h1>
     <h2>Vite React</h2>
     <p>Vite is a build tool that aims to provide a faster and leaner development experience for modern web projects.</p>
     <p>It is designed to be fast, efficient, and easy to use, making it a popular choice among developers.</p>
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
+function MyApp() {
+    return(
+        <div>
+            <h1>Custom App</h1>
+        </div>
+    )
+}
+/*
+const reactElement = {
+  type: 'a',
+  props: {
+    href: 'https://www.google.com',
+    target: '_blank',
+  },
+  children: 'Click me to visit Google'
+}
+*/
+
+
+const anotherElement = (
+    <a href="https://www.youtube.com"
+       target="_blank">
+        Click me to visit Youtube
+        
+    </a>
 )
+const anotherUser = "Roshni Raut"
+   const reactElement = React.createElement(
+    'a',
+    {
+        href: 'https://www.google.com',
+        target: "_blank" },
+        'Click me to visit Google',
+        anotherUser
+   )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  reactElement
+//    <App/>
+  
+) 
