a0fb15206ef7a0b1f2f4616bb63c2bdb115a618a
KillerOp <ribeshraut@gmail.com>
2025-06-20 16:01:15 +0545

    Add counter value boundaries (0 to 20) with alert notifications

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,31 +1,33 @@
+
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
 // function App() {
-//   // let counter = 15;
-// const [counter, setCounter] = useState(15)
-// const addValue = () => {
-//   console.log('clicked',counter())
-//   counter = counter + 1;
- 
-// }
+//   let [counter, setCounter] = useState(15)
+
+//   const addValue = () => {
+//     console.log('clicked', counter)
+//     setCounter(counter + 1)
+//   }
+
+//   const removeValue = () => {
+//     setCounter(counter - 1)
+//   }
+
 //   return (
 //     <>
 //       <h1>Ribesh Raut</h1>
 //       <h2>Counter value: {counter}</h2>
-//       <button onClick={addValue()}>Add value</button>
+//       <button onClick={addValue}>Add value</button>
 //       <br/>
-//       <button>Remove value</button>
+//       <button onClick={removeValue}>Remove value</button>
 //     </>
 //   )
 // }
 
 // export default App
-
-
-
 
 
 
@@ -35,15 +37,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let [counter, setCounter] = useState(15)
+  const [counter, setCounter] = useState(15)
 
   const addValue = () => {
-    console.log('clicked', counter)
-    setCounter(counter + 1)
+    if (counter < 20) {
+      setCounter(counter + 1)
+    } else {
+      alert("Counter can't go above 20!")
+    }
   }
 
   const removeValue = () => {
-    setCounter(counter - 1)
+    if (counter > 0) {
+      setCounter(counter - 1)
+    } else {
+      alert("Counter can't go below 0!")
+    }
   }
 
   return (
